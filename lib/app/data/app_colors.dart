d2bcc7b1e4d8c68c751082d260b2930076880747
mst-borsha <mstborsha852005@gmail.com>
2025-12-06 16:46:54 +0600

    signIn screen not complete ,need merge with miyam

--- lib/app/data/app_colors.dart
+++ lib/app/data/app_colors.dart
@@ -3,6 +3,10 @@ import 'dart:ui';
 class AppColors{
   static const Color whiteColor = Color(0xffFFFFFF);
   static const Color blackColor = Color(0xff000000);
+  static const Color textColor = Color(0xffAFAFAF);
+  static const Color dividerColor = Color(0xff3C3C3C);
+  static const Color topHintColor = Color(0xff606060);
+  static const Color secondaryBlackColor = Color(0xff131313);
   static const Color primaryColor = Color(0xff252525);
   static const Color secondaryColor = Color(0xffF92C67);
   static const Color starFleetBlueColor = Color(0xff0295FC);
--- lib/app/modules/sign_in/views/sign_in_view.dart
+++ lib/app/modules/sign_in/views/sign_in_view.dart
@@ -1,26 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'package:get/get.dart';
+import 'package:yasinmusa/app/data/app_colors.dart';
+import 'package:yasinmusa/app/data/app_text_styles.dart';
 
+import '../../common_widgets/custom_text_field.dart';
 import '../controllers/sign_in_controller.dart';
 
 class SignInView extends GetView<SignInController> {
   const SignInView({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-
-      body: SafeArea(child:
-          Column(
-            children: [
-
-
-
-            ],
-          )
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.sp),
+        child: SafeArea(
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 40.h, horizontal: 16.h),
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: AppColors.secondaryBlackColor,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text('Sign In', style: AppTextStyles.SpaceGroteskBold32),
+                Text(
+                  'Access your account to continue.',
+                  style: AppTextStyles.regular16.copyWith(
+                    color: AppColors.textColor,
+                  ),
+                ),
+                SizedBox(height: 30.w),
+                CustomTextField(
+                  hintText: 'youremail@here',
+                  hintTopText: 'Email',
+                ),
+                CustomTextField(
+                  hintText: '**********',
+                  hintTopText: 'Password',
+                  suffixIcon: Icon(Icons.visibility_off_outlined),
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    'Forgot password?',
+                    style: AppTextStyles.bold16.copyWith(
+                      color: AppColors.starFleetBlueColor,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 32.w),
+                SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Sign In'),
+                  ),
+                ),
+                Padding(
+                  padding:  EdgeInsets.symmetric(vertical: 32.h),
+                  child: Row(
+                    children: [
+                      Expanded(child: Divider(color: AppColors.dividerColor, height: 1)),
+                      Padding(
+                        padding:  EdgeInsets.symmetric(horizontal: 8.w),
+                        child: Text(
+                          'Or',
+                          style: AppTextStyles.regular16.copyWith(
+                            color: AppColors.dividerColor,
+                          ),
+                        ),
+                      ),
+                      Expanded(child: Divider(color: AppColors.dividerColor,height: 1,)),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
-
     );
   }
 }
