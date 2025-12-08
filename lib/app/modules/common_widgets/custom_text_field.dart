b00d38c7ce17e8fc31e1864eb93734f9d94f4054
mst-borsha <mstborsha852005@gmail.com>
2025-12-08 10:45:39 +0600

    custom textfield update with prefixicon

--- lib/app/modules/common_widgets/custom_text_field.dart
+++ lib/app/modules/common_widgets/custom_text_field.dart
@@ -1,22 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:yasinmusa/app/data/app_colors.dart';
+import 'package:yasinmusa/app/data/app_text_styles.dart';
 
 class CustomTextField extends StatelessWidget {
   final String hintText;
   final Icon? suffixIcon;
   final String hintTopText;
-  const CustomTextField({super.key, required this.hintText,
-    required this.hintTopText, this.suffixIcon});
+  final Icon? prefixIcon;
+  final Color? suffixIconColor;
+  final Color? prefixIconColor;
+
+  const CustomTextField({
+    super.key,
+    required this.hintText,
+    required this.hintTopText,
+    this.suffixIcon, this.prefixIcon, this.suffixIconColor, this.prefixIconColor,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return  Column(
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Padding(
-          padding:  EdgeInsets.only(bottom: 8.h),
-          child: Text(hintTopText,style: TextStyle(color: AppColors.topHintColor),),
+          padding: EdgeInsets.only(bottom: 8.h),
+          child: Text(
+            hintTopText,
+            style: AppTextStyles.regular12.copyWith(
+              color: AppColors.topHintColor,
+            ),
+          ),
         ),
 
         TextFormField(
@@ -25,10 +39,13 @@ class CustomTextField extends StatelessWidget {
             fillColor: AppColors.primaryColor,
             hintText: hintText,
             hintStyle: TextStyle(color: AppColors.whiteColor),
-            suffixIcon: suffixIcon,suffixIconColor: AppColors.whiteColor ,
+            suffixIcon: suffixIcon,
+            suffixIconColor: suffixIconColor ?? AppColors.whiteColor,
+            prefixIcon: prefixIcon,
+            prefixIconColor:prefixIconColor ?? AppColors.whiteColor,
           ),
         ),
-        SizedBox(height: 24.h,),
+        SizedBox(height: 24.h),
       ],
     );
   }
